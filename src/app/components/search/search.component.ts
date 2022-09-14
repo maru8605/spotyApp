7ff7a0e6669a887f696fe141bfac2057a331f1fc
maru8605 <maru8605@hotmail.com>
2022-09-13 23:57:29 -0300

    peticiones api artistas

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  artistas: any[] = [];
+  constructor(private spotify: SpotifyService) {}
 
-  constructor() { }
+  buscar(termino: string) {
+    console.log(termino);
 
-  ngOnInit(): void {
+    this.spotify.getArtist(termino).subscribe((data: any) => {
+      console.log();
+      this.artistas = data;
+    });
   }
-
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import {map} from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,11 +16,35 @@ export class SpotifyService {
 
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQBD2bxiU7mW_y25mHbE1qH3WlQUbvv0nQe5HDJbhtZGeN16vF2TU7DXIRf0Ij7g2lSMhrOH1nO6AtAz14FWdYh-IJEGSbMoQ1MKQcca3Zc5a2PFfXE',
+        'Bearer BQBTyST41Lf0wbmseU6v-OH77JNMXzh0d-4Jl7TLoyXQCjwnU1plvkFk3wNdMzhfA284bDbUFBCvm-Eq1w86ih88AfTUx_Zm-VCdxc_4Q_wO7F-fUoY',
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+      .pipe(map((data: any) => {
+        console.log(data.albums.items)
+        return data.albums.items;
+      }))
       
+  }
+
+  getArtist(termino:string) {
+
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQBTyST41Lf0wbmseU6v-OH77JNMXzh0d-4Jl7TLoyXQCjwnU1plvkFk3wNdMzhfA284bDbUFBCvm-Eq1w86ih88AfTUx_Zm-VCdxc_4Q_wO7F-fUoY',
+    });
+
+    return this.http
+      .get(
+        `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
+        { headers }
+      )
+      .pipe(
+        map((data: any) => {
+          console.log(data.artists.items);
+          return data.artists.items;
+        })
+      );
   }
 }
 
