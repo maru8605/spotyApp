57371e37127a61cf7e2af5dbc55cd6bb355bc1a4
maru8605 <maru8605@hotmail.com>
2022-09-20 00:38:54 -0300

    refactor service

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,39 +12,37 @@ export class SpotifyService {
 
   }
 
-  getNewReleases() {
+  getQuery( query: string) {
+    const URL = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQBTyST41Lf0wbmseU6v-OH77JNMXzh0d-4Jl7TLoyXQCjwnU1plvkFk3wNdMzhfA284bDbUFBCvm-Eq1w86ih88AfTUx_Zm-VCdxc_4Q_wO7F-fUoY',
+        'Bearer BQBhvitJfcrylKtgUrelZz8Oq-cQqd2JwchnrXI2KImoCw4ofGH3iI-xxKiEUOIXOUwpAS9weCvz5G14YUWIYU8hfbMskWEZfVktItsRXrUSw8amLho',
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-      .pipe(map((data: any) => {
-        console.log(data.albums.items)
+    return this.http.get(URL, { headers });
+  }
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases')
+      .pipe( map((data: any) => {
+        console.log(data.albums.items);
         return data.albums.items;
-      }))
+      })
+    );
+  
       
   }
 
   getArtist(termino:string) {
 
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQBTyST41Lf0wbmseU6v-OH77JNMXzh0d-4Jl7TLoyXQCjwnU1plvkFk3wNdMzhfA284bDbUFBCvm-Eq1w86ih88AfTUx_Zm-VCdxc_4Q_wO7F-fUoY',
-    });
-
-    return this.http
-      .get(
-        `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
-        { headers }
-      )
-      .pipe(
-        map((data: any) => {
-          console.log(data.artists.items);
-          return data.artists.items;
-        })
-      );
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(
+      map((data: any) => {
+        console.log(data.artists.items);
+        return data.artists.items;
+      })
+    );
   }
 }
 
