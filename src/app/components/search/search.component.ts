898977b3eee0a7ddcfa3c36d9a9c912d62165f02
maru8605 <maru8605@hotmail.com>
2022-09-20 22:48:31 -0300

    componente loading

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -7,15 +7,18 @@ import { SpotifyService } from 'src/app/services/spotify.service';
   styles: [],
 })
 export class SearchComponent {
-  artistas: any[] = [];
+  artistas : any[] = [];
+  loading : boolean | undefined;
   constructor(private spotify: SpotifyService) {}
 
   buscar(termino: string) {
     console.log(termino);
+    this.loading = true;
 
     this.spotify.getArtist(termino).subscribe((data: any) => {
       console.log();
       this.artistas = data;
+      this.loading = false;
     });
   }
 }
